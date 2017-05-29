5d6ffda4558ef4693986660db24946c917ea24a1
Pantelis Zirinis <zirinisp@Panteliss-MacBook-Pro.local>
2017-05-29 10:31:43 +0100

    Initial Commit

---
+++ PazIAPHelper/PazIAPHelper.h
@@ -0,0 +1,19 @@
+//
+//  PazIAPHelper.h
+//  PazIAPHelper
+//
+//  Created by Pantelis Zirinis on 29/05/2017.
+//  Copyright © 2017 Pantelis Zirinis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PazIAPHelper.
+FOUNDATION_EXPORT double PazIAPHelperVersionNumber;
+
+//! Project version string for PazIAPHelper.
+FOUNDATION_EXPORT const unsigned char PazIAPHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PazIAPHelper/PublicHeader.h>
+
+
